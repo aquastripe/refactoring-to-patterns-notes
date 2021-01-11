9be5c176588ab842b295a60e11210808aa63cbcd
Hao-Ting Li <maniac@csie.io>
2021-01-11 10:13:35 +0800

    feat: add description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Refactoring to Patterns Notes',
-  description: '...',
+  description: '重構－向範式前進 (Refactoring to Patterns) 的學習筆記',
   base: '/refactoring-to-patterns-notes/',
   head: [
     ['meta', { name: "viewport", content: "width=device-width,user-scaleble=0,initial-scale=1.0,maximum-scale=1.0" }],
